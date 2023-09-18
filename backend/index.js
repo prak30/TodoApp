38a429a64aa55798deb6b3a029ae3d022b6539d3
Pranav <katkarpranav4@gmail.com>
2023-09-19 00:58:10 +0530

    installed body-parser

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+app.use(bodyParser.json());
 const port = 3001
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
