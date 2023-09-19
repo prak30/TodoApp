f9d2b07a577eb5eb2eb19c66230a91068f2e8c9e
Pranav <katkarpranav4@gmail.com>
2023-09-20 02:19:17 +0530

    created a todos.json file which saves the data

--- backend/index.js
+++ backend/index.js
@@ -1,65 +1,87 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 app.use(bodyParser.json());
-const port = 3001
-let count =0;
+const port = 3001;
+const todosFilePath = path.join(__dirname, 'todos.json');
+let count = 1;
 
-let todos = [];
-
-app.get('/todos', (req,res) => {
-    res.json(todos);
-})
-
-app.get('/todos/:id', (req,res)=>{
-    let id = parseInt(req.params.id);
-    const todo = todos.find(todo => todo.id === id);
-    if (!todo) {
-        res.status(404).send();
-    } else {
-        res.json(todo);
+// Function to read todos from the file
+function readTodosFromFile() {
+  try {
+    const todosData = fs.readFileSync(todosFilePath, 'utf8');
+    return JSON.parse(todosData);
+  } catch (err) {
+    return [];
   }
-})
+}
 
-app.post('/todos', (req,res)=>{
-    let newTodo = {
-        id: count,
-        title:req.body.title,
-        description:req.body.description
-    }
-    count = count+1
-    todos.push(newTodo);
-    res.status(201).json(newTodo);
-})
+// Function to write todos to the file
+function writeTodosToFile(todos) {
+  fs.writeFileSync(todosFilePath, JSON.stringify(todos,null,2), 'utf8');
+}
 
-app.delete('/todos/:id',(req,res)=>{
-    let id = parseInt(req.params.id);
-    const delIndex = todos.findIndex(todo => todo.id === id)
-    if (delIndex === -1) {
-        res.status(404).send();
-    } else {
-        todos.splice(delIndex, 1);
-        res.status(200).json("todo with request id " + id +  " deleted successfully");
+app.get('/todos', (req, res) => {
+  const todos = readTodosFromFile();
+  res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todos = readTodosFromFile();
+  const id = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === id);
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
   }
-})
+});
+
+app.post('/todos', (req, res) => {
+  const todos = readTodosFromFile();
+  const newTodo = {
+    id: count,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  count = count + 1;
+  todos.push(newTodo);
+  writeTodosToFile(todos);
+  res.status(201).json(newTodo);
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const todos = readTodosFromFile();
+  const id = parseInt(req.params.id);
+  const delIndex = todos.findIndex((todo) => todo.id === id);
+  if (delIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos.splice(delIndex, 1);
+    writeTodosToFile(todos);
+    res.status(204).send(); // 204 No Content for successful deletion
+  }
+});
 
 app.put('/todos/:id', (req, res) => {
-    const todoIndex = todos.findIndex(todo => todo.id === parseInt(req.params.id))
-    if (todoIndex === -1) {
-      res.status(404).send();
-    } else {
-      todos[todoIndex].title = req.body.title;
-      todos[todoIndex].description = req.body.description;
-      res.json(todos[todoIndex]);
-    }
+  const todos = readTodosFromFile();
+  const todoIndex = todos.findIndex((todo) => todo.id === parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    writeTodosToFile(todos);
+    res.json(todos[todoIndex]);
+  }
 });
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not Found' });
-  });
-
-
+  res.status(404).json({ message: 'Not Found' });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
