474eed3c89d1bbfdb55a11cacc005d44842ecfcb
Pranav <katkarpranav4@gmail.com>
2023-09-21 02:06:09 +0530

    added cors and used it

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 const port = 3001;
 
