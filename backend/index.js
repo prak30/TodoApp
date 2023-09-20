84bec3e681736b79823c38e823c36aafba18c0c2
Pranav <katkarpranav4@gmail.com>
2023-09-20 19:33:09 +0530

    optimised by using mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,80 +1,88 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 const port = 3001;
-const todosFilePath = path.join(__dirname, 'todos.json');
-let count = 1;
 
-// Function to read todos from the file
-function readTodosFromFile() {
+//Define mongoose schema
+const todoSchema = new mongoose.Schema({
+    title : String,
+    description : String
+})
+//Define mongoose model
+const Todo = mongoose.model('Todo', todoSchema);
+//connect to mongodb
+mongoose.connect('mongodb+srv://pranavv:katkar@cluster0.oqehcar.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+      } catch (error) {
+        console.error('Error fetching todos:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+});
+
+app.get('/todos/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const todo = await Todo.findById(id);
+      if (!todo) {
+        res.status(404).send();
+      } else {
+        res.json(todo);
+      }
+    } catch (error) {
+      console.error('Error fetching todo by ID:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/todos', async (req, res) => {
+  const {title, description} = req.body
   try {
-    const todosData = fs.readFileSync(todosFilePath, 'utf8');
-    return JSON.parse(todosData);
-  } catch (err) {
-    return [];
-  }
-}
-
-// Function to write todos to the file
-function writeTodosToFile(todos) {
-  fs.writeFileSync(todosFilePath, JSON.stringify(todos,null,2), 'utf8');
-}
-
-app.get('/todos', (req, res) => {
-  const todos = readTodosFromFile();
-  res.json(todos);
-});
-
-app.get('/todos/:id', (req, res) => {
-  const todos = readTodosFromFile();
-  const id = parseInt(req.params.id);
-  const todo = todos.find((todo) => todo.id === id);
-  if (!todo) {
-    res.status(404).send();
-  } else {
-    res.json(todo);
+    const newTodo = new Todo({ title, description });
+    const savedTodo = await newTodo.save();
+    res.status(201).json(savedTodo);
+  } catch (error) {
+    console.error('Error creating todo:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.post('/todos', (req, res) => {
-  const todos = readTodosFromFile();
-  const newTodo = {
-    id: count,
-    title: req.body.title,
-    description: req.body.description,
-  };
-  count = count + 1;
-  todos.push(newTodo);
-  writeTodosToFile(todos);
-  res.status(201).json(newTodo);
+app.delete('/todos/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await Todo.deleteOne({ _id: id });
+      if (result.deletedCount === 0) {
+        res.status(404).send();
+      } else {
+        res.status(204).send();
+      }
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-app.delete('/todos/:id', (req, res) => {
-  const todos = readTodosFromFile();
-  const id = parseInt(req.params.id);
-  const delIndex = todos.findIndex((todo) => todo.id === id);
-  if (delIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos.splice(delIndex, 1);
-    writeTodosToFile(todos);
-    res.status(204).send(); // 204 No Content for successful deletion
-  }
-});
-
-app.put('/todos/:id', (req, res) => {
-  const todos = readTodosFromFile();
-  const todoIndex = todos.findIndex((todo) => todo.id === parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos[todoIndex].title = req.body.title;
-    todos[todoIndex].description = req.body.description;
-    writeTodosToFile(todos);
-    res.json(todos[todoIndex]);
+app.put('/todos/:id', async (req, res) => {
+    const id = req.params.id;
+    const { title, description } = req.body;
+    try {
+        const updatedTodo = await Todo.findByIdAndUpdate(id, { title, description }, { new: true });
+        if (!updatedTodo) {
+         res.status(404).send();
+        } else {
+        res.json(updatedTodo);
+    }
+    } catch (error) {
+    console.error('Error updating todo:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
